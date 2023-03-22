844be82508221df7f73a8f33d0b89b5816898f88
kevinkindler <kevin.kindler@uzh.ch>
2023-03-22 17:07:45 +0100

    loginPage: background image and first draft of form

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -1,11 +1,68 @@
 import React from 'react';
+import { Button, Checkbox, Form, Input } from 'antd';
+import styled from 'styled-components'
+import image from './components/images/dog.png'
 
-const Login = () => {
-    return (
-        <div>
-            <h1>Sign Up Successful</h1>
-        </div>
-    );
+const FormContainer = styled.div`
+    maring-top: 0%;
+    flex: 0 0 50%;
+    display: flex;
+    align-items: center;
+    min-height: 700px;
+    justify-content: center;
+    background: transparent;
+`;
+
+const onFinish = (values: any) => {
+    console.log('Success:', values);
 };
+
+const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+};
+
+const Login: React.FC = () => (
+    <div style={{ backgroundImage:`url(${image})` }}>
+
+        <FormContainer>
+            <Form
+                name="basic"
+                labelCol={{ span: 8 }}
+                wrapperCol={{ span: 16 }}
+                style={{ maxWidth: 600 }}
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
+            >
+                <Form.Item
+                    label="Username"
+                    name="username"
+                    rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[{ required: true, message: 'Please input your password!' }]}
+                >
+                    <Input.Password />
+                </Form.Item>
+
+                <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+                    <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+
+                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+        </FormContainer>
+    </div>
+);
 
 export default Login;
