55555d555374198429590d2466871ec6abee8dff
kevinkindler <kevin.kindler@uzh.ch>
2023-05-11 14:27:29 +0200

    posting images working!

--- frontend/src/components/pages/mainPageComponents/newsFeed.js
+++ frontend/src/components/pages/mainPageComponents/newsFeed.js
@@ -356,7 +356,7 @@ function Posts(){
                                             title: responseDataOfRetrievePost[i].title,
                                             name: responseDataOfGetUsername.username,
                                             text: responseDataOfRetrievePost[i].content,
-                                            file: "http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcRPMKnq00NF_T7RusUNeLrSazRZM0S5O8_AOcw2iBTmYTxd3Q7uXf0sW41odpAKqSblKDMUMHGb8nZRo9g",
+                                            file: "/images/" + responseDataOfRetrievePost[i].image,
                                             comments: commentsByPost,
                                             likes: responseDataOfRetrievePost[i].likes,
 
